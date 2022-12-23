8546f7d455e00921fe450cbafc78bc7e953eb94d
VijayP-7 <108809593+VijayP-7@users.noreply.github.com>
2022-12-23 13:29:48 +0530

    Update Assignment.sql

--- Assignment.sql
+++ Assignment.sql
@@ -60,12 +60,12 @@ INSERT INTO ORDERS VALUES
 SELECT * FROM ORDERS;
 
 
-#Querry No:- Count the number of Salesperson whose name begin with ‘a’/’A’.
+#Querry No 1:- Count the number of Salesperson whose name begin with ‘a’/’A’.
 SELECT COUNT(SNAME)
 FROM SALESPEOPLE
 WHERE SNAME LIKE 'a%' OR 'A%';
 
-#  Querry No:- Display all the Salesperson whose all orders worth is more than Rs. 2000.
+#  Querry No 2:- Display all the Salesperson whose all orders worth is more than Rs. 2000.
 SELECT S.SNAME, O.AMT
 FROM SALESPEOPLE S
 LEFT JOIN ORDERS O
@@ -73,18 +73,18 @@ ON S.SNUM = O.SNUM
 WHERE O.AMT > 2000
 ORDER BY O.AMT;
 
-# Querry No3:- Count the number of Salesperson belonging to Newyork.
+# Querry No 3:- Count the number of Salesperson belonging to Newyork.
 SELECT COUNT(*) NYBASED
 FROM SALESPEOPLE
 WHERE CITY = 'NEWYORK';
 
-#  Display the number of Salespeople belonging to London and belonging to Paris.
+#  Querry No 4:-Display the number of Salespeople belonging to London and belonging to Paris.
 SELECT *
 FROM SALESPEOPLE 
 WHERE CITY= 'LONDON' OR CITY = 'PARIS';
 
 
-# Display the number of orders taken by each Salesperson and their date of orders.
+# Querry No 5:-Display the number of orders taken by each Salesperson and their date of orders.
 SELECT S.SNAME,O.ODATE,COUNT(O.ODATE) AS NO_OF_ORDERS
 FROM SALESPEOPLE S
 LEFT JOIN ORDERS O
